597c9572fe23980dc0aa247627bb285615fbd4fa
PaulVagner <prodigyzro@gmail.com>
2015-10-19 09:25:55 -0400

    Initial Create
    App Created

---
+++ Camera/Camera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Camera
+//
+//  Created by Paul Vagner on 10/19/15.
+//  Copyright © 2015 Paul Vagner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

bcca708d79adb6c4e4a75f02ae95b6f4288eb2e8
PaulVagner <prodigyzro@gmail.com>
2015-10-19 14:15:18 -0400

    playing with the camera

--- Camera/Camera/ViewController.h
+++ Camera/Camera/ViewController.h
@@ -10,6 +10,12 @@
 
 @interface ViewController : UIViewController
 
+//@property NSArray * names;
+//@property (readonly) NSArray * boxes; //SWIFT: let boxes: NSArray!
+////this is accessable by any other class
+//
+//- (UIView *)createBox:(CGRect)frame; //SWIFT: func crateBox(frame: CGRect) -> UIView
+//// "-" denotes wether it is an instance method, "+" denotes class method
 
 @end
 
--- Camera/Camera/ViewController.m
+++ Camera/Camera/ViewController.m
@@ -1,27 +1,163 @@
+////
+////  ViewController.m
+////  Camera
+////
+////  Created by Paul Vagner on 10/19/15.
+////  Copyright © 2015 Paul Vagner. All rights reserved.
+////
 //
-//  ViewController.m
-//  Camera
+//#import "ViewController.h"
 //
-//  Created by Paul Vagner on 10/19/15.
-//  Copyright © 2015 Paul Vagner. All rights reserved.
+//@interface ViewController ()
 //
+//@end
+//
+////@implementation ViewController
+////
+////- (UIView *)createBox:(CGRect)frame {
+////    // "*" <- denotes that it is a pointer
+////    UIView * view = [[UIView alloc] initWithFrame:frame];
+////    //SWIFT: let view = UIView(frame: frame)
+////
+////
+////    //This points to the same exact location in memory as UIView * view = [[UIView alloc] initWithFrame:frame]
+////    UIView * view2 = view;
+////
+////    view = [[UIView alloc] init];
+////
+////
+////    int num1 = 5;
+////
+////    int num2 = num1;
+////
+////
+////    return view;
+////
+////}
+//
+//@implementation ViewController
+//
+//@synthesize names=_names;
+//
+//- (void)setNames:(NSArray *)names {
+////SETTER method "_" <- expresses that it is an instance variable
+//    _names = names;
+//
+//}
+//
+//- (NSArray *)names {
+//
+//    if (_names == nil) { _names = @[]; }
+//
+//    //GETTER method
+//    return _names;
+//
+//}
+//
+//- (void)viewDIDLoad {
+//    [super viewDidLoad];
+//
+//   // self.names = @[@"Mac", @"Joe", @"Kelly", @"Paul"];
+//
+//    NSArray * myNames = self.names;
+//
+//    _names = @[@"Mac", @"Joe", @"Kelly", @"Paul"];
+//
+//}
+//
+//
+//@end
+//
+
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import <MobileCoreServices/MobileCoreServices.h>
+
+
+
+@interface ViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+
+@property UIImagePickerController * pickerController;
+
+@property (weak, nonatomic) IBOutlet UIImageView * mySelfieImageView;
+
+
+
+
 
 @end
 
 @implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//initWithCoder only initialized when it starts in storyboard
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    
+    self = [super initWithCoder:aDecoder];
+    
+    if (self != nil) {
+        
+        self.pickerController = [[UIImagePickerController alloc] init];
+    
+        self.pickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+        
+        self.pickerController.cameraDevice = UIImagePickerControllerCameraDeviceFront;
+        
+        
+        self.pickerController.delegate = self;
+        
+        
+        
+        
+    }
+    
+    return self;
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+     
+
+    
+//    [self.view addSubview:self.pickerController.view];
+
+    if (self.mySelfieImageView.image == nil) {
+    
+    [self presentViewController:self.pickerController animated:YES completion:nil];
+    
+    }
+}
+
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    NSString * type = info[UIImagePickerControllerMediaType];
+    
+    if (type == (NSString *)kUTTypeImage) {
+      
+        UIImage * image = info[UIImagePickerControllerOriginalImage];
+        
+        self.mySelfieImageView.image = image;
+        
+        
+        
+    }
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
+    
+    
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
